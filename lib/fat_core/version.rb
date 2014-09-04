a4a71be3aa1fad61cae49a2400455eeac0ecfcd5
Daniel E. Doherty <ded-law@ddoherty.net>
2014-09-04 05:19:42 -0500

    Fix version string without BUILD.

--- lib/fat_core/version.rb
+++ lib/fat_core/version.rb
@@ -2,7 +2,6 @@ module FatCore
   MAJOR = 0
   MINOR = 1
   PATCH = 2
-  BUILD = ''
 
-  VERSION = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
+  VERSION = [MAJOR, MINOR, PATCH].compact.join('.')
 end
