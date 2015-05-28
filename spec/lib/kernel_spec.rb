7279e8efa0db26bd5a5cfd9fc69d63ecea820232
Daniel E. Doherty <ded-law@ddoherty.net>
2015-05-28 06:48:41 -0500

    Send STDOUT to a string.

--- spec/lib/kernel_spec.rb
+++ spec/lib/kernel_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Kernel do
   it "should know how to time a block of code" do
+    $stdout = StringIO.new
     result = time_it "do something" do
       (1..10_000_000).each { |k| k * k }
       "hello"
