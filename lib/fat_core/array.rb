289e2e8361094def258a72384091e8a85d55d984
Daniel E. Doherty <ded-law@ddoherty.net>
2017-03-02 15:42:35 -0600

    Add intersect and difference for Array without de-duping.
    The library methods :& and :- for Arrays remove duplicates, and we want this to
    be optional for Table set operations.

--- lib/fat_core/array.rb
+++ lib/fat_core/array.rb
@@ -2,4 +2,26 @@ class Array
   def last_i
     self.size - 1
   end
+
+  # Return a new array that is the intersection of this Array with other, but
+  # without removing duplicates as the :& method does.
+  def intersect(other)
+    result = []
+    each do |itm|
+      result << itm if other.include?(itm)
+    end
+    result
+  end
+
+  # Return an array that is the difference between this Array and the other, but
+  # without removing duplicates as the :- method does.
+  def difference(other)
+    result = deep_dup
+    other.each do |itm|
+      if (k = result.index(itm))
+        result.delete_at(k)
+      end
+    end
+    result
+  end
 end
--- spec/lib/array_spec.rb
+++ spec/lib/array_spec.rb
@@ -5,4 +5,20 @@ describe Array do
     letters = ('a'..'z').to_a
     expect(letters.last_i).to eq(25)
   end
+
+  it 'intersection' do
+    expect(%w(A A B A C A B).intersect(%w(A B A))).to eq(%w(A A B A A B))
+    expect(%w(A A B A C A B).intersect(%w(A B))).to eq(%w(A A B A A B))
+    expect(%w(A A B A C A B).intersect(%w(A))).to eq(%w(A A A A))
+    expect(%w(A A B A C A B).intersect(%w(B))).to eq(%w(B B))
+    expect(%w(A A B A C A B).intersect(%w(C))).to eq(%w(C))
+  end
+
+  it 'difference' do
+    expect(%w(A A B A C A B).difference(%w(A B A))).to eq(%w(A C A B))
+    expect(%w(A A B A C A B).difference(%w(A B))).to eq(%w(A A C A B))
+    expect(%w(A A B A C A B).difference(%w(A))).to eq(%w(A B A C A B))
+    expect(%w(A A B A C A B).difference(%w(B))).to eq(%w(A A A C A B))
+    expect(%w(A A B A C A B).difference(%w(C))).to eq(%w(A A B A A B))
+  end
 end
