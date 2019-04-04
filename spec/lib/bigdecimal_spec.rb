dd9c932cf80379da32310f0774862c9cb4e1d581
Daniel E. Doherty <ded-law@ddoherty.net>
2019-04-04 12:19:08 -0500

    Remove deprecated BigDecimal.new on move to ruby 2.6.2.

--- spec/lib/bigdecimal_spec.rb
+++ spec/lib/bigdecimal_spec.rb
@@ -2,6 +2,6 @@ require 'fat_core/bigdecimal'
 
 describe BigDecimal do
   it 'should provide a human-readable inspect for BigDecimal' do
-    expect(BigDecimal.new('33.45').inspect).to eq '33.45'
+    expect(BigDecimal('33.45').inspect).to eq '33.45'
   end
 end
