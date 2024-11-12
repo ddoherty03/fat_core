00f19b15070a0647702afaa09dce16f99c0f4df9
Daniel E. Doherty <ded@ddoherty.net>
2024-11-12 04:40:21 -0600

    Require 'date'.

--- lib/fat_core.rb
+++ lib/fat_core.rb
@@ -1,5 +1,6 @@
 require 'fat_core/version'
 require 'fat_core/patches'
 require 'active_support'
+require 'date'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/object/deep_dup'
