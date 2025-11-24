f08eeef3a89dc7241a31be3ad9c0d8a37fa1267b
Daniel E. Doherty <ded@ddoherty.net>
2025-11-24 05:41:06 -0600

    Remove Enumerable#groups_of as duplicative of #each_slice.

--- lib/fat_core/enumerable.rb
+++ lib/fat_core/enumerable.rb
@@ -2,21 +2,6 @@
 
 # Useful extensions to the core Enumerable module.
 module Enumerable
-  # Yield items in groups of n, for each group yield the group number, starting
-  # with zero and an Array of n items, or all remaining items if less than n.
-  #
-  #    ('a'..'z').to_a.groups_of(5) do |k, grp|
-  #      # On each iteration, grp is an Array of the next 5 items except the
-  #      # last group, which contains only ['z'].
-  #    end
-  def groups_of(num)
-    k = -1
-    group_by do
-      k += 1
-      k.div(num)
-    end
-  end
-
   # Yield each item together with two booleans that indicate whether the item is
   # the first or last item in the Enumerable.
   #
