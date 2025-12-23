f125c32d4f11678a945e6ea1b5b76c5e93452aff
Daniel E. Doherty <ded@ddoherty.net>
2025-12-23 11:03:08 -0600

    Run docs:all.

--- lib/fat_core.rb
+++ lib/fat_core.rb
@@ -3,5 +3,18 @@
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/object/deep_dup'
 
-require 'fat_core/version'
-require 'fat_core/patches'
+
+# Gem Overview (extracted from README.org by gem_docs)
+#
+#
+# * Introduction
+#
+# ~fat-core~ is somewhat of a grab bag of core class extensions that I have
+# found useful across several projects.  It's higgeldy-piggeldy nature reflects
+# the fact that none of them are important enough to deserve a gem of their own,
+# but nonetheless need to be collected in one place to reduce redundancy across
+# projects and provide a focused place to develop and test them.
+module FatCore
+  require 'fat_core/version'
+  require 'fat_core/patches'
+end
